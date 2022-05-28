d6d018bee3e7d0011733cafd78152e961d74fe6d
Abdul-Samii <ksamk100474@gmail.com>
2022-05-28 16:55:49 +0500

    Success bar issue solve

--- src/store/actions/AuthAction.js
+++ src/store/actions/AuthAction.js
@@ -45,7 +45,7 @@ export const UserLogin = (data) => async dispatch => {
         response = await httpRequest.post('/login/', data,{
             headers: { "content-type": 'multipart/form-data' }
         }).catch((err)=>{
-            console.log(err.request.response)
+            progress.finish()
             dispatch(setToast('error',err.request.response))
         })
         dispatch({type:types.LOGIN_FAILED})
